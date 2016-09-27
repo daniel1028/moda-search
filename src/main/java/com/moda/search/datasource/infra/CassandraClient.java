a632760d47ab5ff664d45fcb3a2a414f7f93f01f
Daniel <arokiadaniel.a@bambeeq.com>
2016-09-27 16:09:31 +0530

    initial commit

---
+++ src/main/java/com/moda/search/datasource/infra/CassandraClient.java
@@ -0,0 +1,44 @@
+package com.moda.search.datasource.infra;
+
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class CassandraClient {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CassandraClient.class);
+
+  @Resource(name = "cassandra")
+  private Properties cassandra;
+
+  public static CassandraClient instance() {
+    return CassandraClientHolder.INSTANCE;
+  }
+
+  private static final class CassandraClientHolder {
+    public static final CassandraClient INSTANCE = new CassandraClient();
+  }
+
+  @PostConstruct
+  public void initialize() {
+    
+    LOG.info("Loading properties");
+
+    String hosts = cassandra.getProperty("analytics.cassandra.seeds");
+
+    String clusterName = cassandra.getProperty("analytics.cassandra.cluster");
+
+    LOG.info("hosts:" + hosts);
+    LOG.info("clusterName:" + clusterName);
+  }
+  
+  @PreDestroy
+  public void finalize(){
+    LOG.info("Closing connections....");
+  }
+}
